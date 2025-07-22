d3197838ddf8cbb80eb33285e80b73cc03909620
GibranKhalil <gibrankhalil150904@gmail.com>
2025-07-21 23:08:02 -0300

    FEAT: aplicação de controles de seleção de personagem

--- src/constants/characters.constant.js
+++ src/constants/characters.constant.js
@@ -4,22 +4,28 @@ const CHARACTERS_CONFIG = {
     NINJA_GREEN: {
         Faceset: `${ASSETS_PATH.Characters}/NinjaGreen/Faceset.png`,
         SpriteSheet: `${ASSETS_PATH.Characters}/NinjaGreen/SpriteSheet.png`,
-        Color: Color.new(86, 134, 76)
+        Color: Color.new(86, 134, 76),
+        Name: "GREEN  NINJA"
     },
     PIG: {
         Faceset: `${ASSETS_PATH.Characters}/Pig/Faceset.png`,
         SpriteSheet: `${ASSETS_PATH.Characters}/Pig/SpriteSheet.png`,
-        Color: Color.new(121, 184, 206)
+        Color: Color.new(242, 173, 125),
+        Name: "PIGGY"
+
     },
     SKELETON: {
         Faceset: `${ASSETS_PATH.Characters}/Skeleton/Faceset.png`,
         SpriteSheet: `${ASSETS_PATH.Characters}/Skeleton/SpriteSheet.png`,
-        Color: Color.new(242, 173, 125)
+        Color: Color.new(242, 234, 241),
+        Name: "SKELETON"
     },
     SPIRIT: {
         Faceset: `${ASSETS_PATH.Characters}/Spirit/Faceset.png`,
         SpriteSheet: `${ASSETS_PATH.Characters}/Spirit/SpriteSheet.png`,
-        Color: Color.new(242, 234, 241)
+        Color: Color.new(121, 184, 206),
+        Name: "SPIRIT"
+
     },
 }
 
--- src/views/screens/PlayerSelect.screen.js
+++ src/views/screens/PlayerSelect.screen.js
@@ -21,46 +21,35 @@ export default class PlayerSelectScreen extends BaseScreen {
         this.footer = new Image(`${ASSETS_PATH.Components}/player-select-footer.png`)
         this.pattern = new Image(`${ASSETS_PATH.Backgrounds}/pattern.png`)
 
-        this.playerOneText = "NINJA"
-        this.playerTwoText = "BLUE  FIRE"
-        this.playerThreeText = "PIGGY"
-        this.playerFourthText = "SKELETON"
-
-        this.faceset = new Image(`${ASSETS_PATH.Characters}/NinjaGreen/Faceset.png`)
-        this.playerTwoFaceset = new Image(`${ASSETS_PATH.Characters}/Spirit/Faceset.png`)
-        this.playerThreeFaceset = new Image(`${ASSETS_PATH.Characters}/Pig/Faceset.png`)
-        this.playerFourthFaceset = new Image(`${ASSETS_PATH.Characters}/Skeleton/Faceset.png`)
-
-        this.playerOneCharacter = new Image(`${ASSETS_PATH.Characters}/NinjaGreen/SpriteSheet.png`)
-        this.playerTwoCharacter = new Image(`${ASSETS_PATH.Characters}/Spirit/SpriteSheet.png`)
-        this.playerThreeCharacter = new Image(`${ASSETS_PATH.Characters}/Pig/SpriteSheet.png`)
-        this.playerFourthCharacter = new Image(`${ASSETS_PATH.Characters}/Skeleton/SpriteSheet.png`)
-
         this.arrow = new Image(`${ASSETS_PATH.Components}/arrow.png`)
         this.arrow.height = 32;
+
+        this.selectedCharacter = 0;
+        this.selectedCharacter2 = 0;
+        this.characters = Object.values(CHARACTERS_CONFIG)
+
+        this.pad = Pads.get(0);
+        this.pad2 = Pads.get(1);
     }
 
     render() {
         this.renderBackground();
+        this.pad.update();
+        this.pad2.update();
 
         this.headerFrame.draw(22, 10)
 
-        Draw.rect(20, 93, 132, 305, CHARACTERS_CONFIG.NINJA_GREEN.Color);
+        this.faceset = new Image(this.characters[this.selectedCharacter].Faceset)
+        this.playerOneCharacter = new Image(this.characters[this.selectedCharacter].SpriteSheet)
+        Draw.rect(20, 93, 132, 305, this.characters[this.selectedCharacter].Color);
         this.pattern.draw(20, 93)
 
-        Draw.rect(177, 93, 132, 305, CHARACTERS_CONFIG.SPIRIT.Color);
-        this.pattern.draw(177, 93)
-
-        Draw.rect(331, 93, 132, 305, CHARACTERS_CONFIG.PIG.Color);
-        this.pattern.draw(331, 93)
-
-        Draw.rect(486, 93, 132, 305, CHARACTERS_CONFIG.SKELETON.Color);
-        this.pattern.draw(486, 93)
+        this.faceset2 = new Image(this.characters[this.selectedCharacter2].Faceset)
+        this.player2Character = new Image(this.characters[this.selectedCharacter2].SpriteSheet)
+        Draw.rect(160, 93, 132, 305, this.characters[this.selectedCharacter2].Color);
+        this.pattern.draw(160, 93)
 
         this.playerSelectFrame.draw(14, 76)
-        this.playerSelectFrame.draw(171, 76)
-        this.playerSelectFrame.draw(325, 76)
-        this.playerSelectFrame.draw(480, 76)
 
         this.arrow.width = -Math.abs(32);
         this.arrow.draw(36, 208)
@@ -68,77 +57,74 @@ export default class PlayerSelectScreen extends BaseScreen {
         this.arrow.width = Math.abs(32);
         this.arrow.draw(136, 208)
 
-        this.arrow.width = -Math.abs(32);
-        this.arrow.draw(192, 208)
+        if (this.pad.justPressed(Pads.LEFT)) {
+            if (this.selectedCharacter < 1) {
+                this.selectedCharacter = this.characters.length - 1
+            }
+            else {
+                this.selectedCharacter--;
+            }
+        }
 
-        this.arrow.width = Math.abs(32);
-        this.arrow.draw(296, 208)
+        if (this.pad.justPressed(Pads.RIGHT)) {
+            if (this.selectedCharacter >= this.characters.length - 1) {
+                this.selectedCharacter = 0
+            }
+            else {
+                this.selectedCharacter++;
+            }
+        }
 
-        this.arrow.width = -Math.abs(32);
-        this.arrow.draw(348, 208)
+        if (this.pad2.justPressed(Pads.LEFT)) {
+            if (this.selectedCharacter2 < 1) {
+                this.selectedCharacter2 = this.characters.length - 1
+            }
+            else {
+                this.selectedCharacter2--;
+            }
+        }
 
-        this.arrow.width = Math.abs(32);
-        this.arrow.draw(452, 208)
+        if (this.pad2.justPressed(Pads.RIGHT)) {
+            if (this.selectedCharacter2 >= this.characters.length - 1) {
+                this.selectedCharacter2 = 0
+            }
+            else {
+                this.selectedCharacter2++;
+            }
+        }
 
-        this.arrow.width = -Math.abs(32);
-        this.arrow.draw(504, 208)
 
-        this.arrow.width = Math.abs(32);
-        this.arrow.draw(608, 208)
-
-
-        AnimatorSystem.animationByColumns(4, 1, 4, 16, 16, true, this.playerOneCharacter, 2)
+        this.playerOneCharacter.startx = 0;
+        this.playerOneCharacter.endx = 16;
+        this.playerOneCharacter.starty = 0;
+        this.playerOneCharacter.endy = 16;
+        this.playerOneCharacter.width = 16 * 2;
+        this.playerOneCharacter.height = 16 * 2;
         this.playerOneCharacter.draw(70, 208)
 
-        AnimatorSystem.animationByColumns(4, 1, 4, 16, 16, true, this.playerTwoCharacter, 2)
-        this.playerTwoCharacter.draw(226, 208)
+        this.player2Character.startx = 0;
+        this.player2Character.endx = 16;
+        this.player2Character.starty = 0;
+        this.player2Character.endy = 16;
+        this.player2Character.width = 16 * 2;
+        this.player2Character.height = 16 * 2;
+        this.player2Character.draw(160, 208)
 
-        AnimatorSystem.animationByColumns(4, 1, 4, 16, 16, true, this.playerThreeCharacter, 2)
-        this.playerThreeCharacter.draw(382, 208)
 
-        AnimatorSystem.animationByColumns(4, 1, 4, 16, 16, true, this.playerFourthCharacter, 2)
-        this.playerFourthCharacter.draw(538, 208)
-
-        AnimatorSystem.animationByRows(1, 7, 4, 64, 16, true, this.playerOneCharacter)
+        AnimatorSystem.animationByRows(1, 4, 4, 64, 16, true, this.playerOneCharacter)
         this.playerOneCharacter.draw(75, 369)
 
-        AnimatorSystem.animationByRows(1, 7, 4, 64, 16, true, this.playerTwoCharacter)
-        this.playerTwoCharacter.draw(227, 369)
-
-        AnimatorSystem.animationByRows(1, 7, 4, 64, 16, true, this.playerThreeCharacter)
-        this.playerThreeCharacter.draw(383, 369)
-
-        AnimatorSystem.animationByRows(1, 7, 4, 64, 16, true, this.playerFourthCharacter)
-        this.playerFourthCharacter.draw(539, 369)
+        AnimatorSystem.animationByRows(1, 4, 4, 64, 16, true, this.player2Character)
+        this.player2Character.draw(165, 369)
 
         this.faceset.draw(20, 358)
-        this.playerTwoFaceset.draw(177, 358)
-        this.playerThreeFaceset.draw(331, 358)
-        this.playerFourthFaceset.draw(486, 358)
+        this.faceset2.draw(160, 358)
 
         this.players_num.startx = 0
         this.players_num.endx = 16
         this.players_num.width = 16
         this.players_num.height = 16
         this.players_num.draw(145, 389)
-
-        this.players_num.startx = 16
-        this.players_num.endx = 32
-        this.players_num.width = 16
-        this.players_num.height = 16
-        this.players_num.draw(301, 389)
-
-        this.players_num.startx = 32
-        this.players_num.endx = 48
-        this.players_num.width = 16
-        this.players_num.height = 16
-        this.players_num.draw(457, 389)
-
-        this.players_num.startx = 48
-        this.players_num.endx = 64
-        this.players_num.width = 16
-        this.players_num.height = 16
-        this.players_num.draw(613, 389)
 
         this.footer.draw(0, 418)
 
@@ -148,16 +134,10 @@ export default class PlayerSelectScreen extends BaseScreen {
 
         this.font.color = COLORS.BLACK
         this.font.scale = FONT_SIZE.XS
-        this.font.print(72, 87, this.playerOneText)
-        this.font.print(220, 87, this.playerTwoText)
-        this.font.print(382, 87, this.playerThreeText)
-        this.font.print(528, 87, this.playerFourthText)
+        this.font.print(72, 87, this.characters[this.selectedCharacter].Name)
 
         this.font.color = COLORS.WHITE
         this.font.scale = FONT_SIZE.SM
         this.font.print(150, 393, "1")
-        this.font.print(307, 393, "2")
-        this.font.print(462, 393, "3")
-        this.font.print(617, 393, "4")
     }
 }
